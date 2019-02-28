0c3fc156f2ee3f5c17bb76bac35205f2a367bbb9
Casey Berman <casberma@publicisgroupe.net>
2019-02-28 13:54:21 -0500

    simplified puppetee_server (no longer using mongo), added region folders

--- puppeteer_server.js
+++ puppeteer_server.js
@@ -4,18 +4,6 @@ const fs = require('fs');
 const mongoConnect = require('./mongoconnect');
 
 
-// game plan:
-// 1: instead of saving to a flat array, create/check an object
-// split url by forward slash, check if that portion of the
-// object exists, if not, create it. (check!)
-// 2: separate these into two files maybe? one for the "getting" (maybe save to json file for now) and one for the crawling (check!)
-// 3: time stamp the file name or folder?
-// 4: implement try/catch in upper function
-// 4: still discussing this "series.json" business
-// 3: be able to truly handle errors - throwing, catching, all that
-// 5: 'actually crawling' - the homepage, get every link, compile objectify, compile etc
-// 6: MongoDb functionality (check!)
-// 7: lightweight angular UI to display photos (as they appear?) (check!)
 // ---------------------------------------- url looper ----------------------------------------------------
 
 // in case you forget: for some reason, puppeteer seems to have trouble when the url contains a '/' at 
@@ -24,21 +12,19 @@ const mongoConnect = require('./mongoconnect');
 
 async function getUrlObj(region, mobile) {
 
-    // objectFromMongo
+    // objectFromMongo or json
     let arrayFromUrlObject = (obj, str, arr) => {
         let myAccumulator = arr;
         for (key in obj) {
             if (!obj.hasOwnProperty(key)) {
                 continue;
             };
-            // let currentUrl = Object.getOwnPropertyNames(obj); // this is not being used... why? what? 
             let nextString = str + "/" + key;
             if(str == "") {
                 nextString = nextString.substring(1);
             }
             let regex = /_-_-z-_-_/g;
             nextString = nextString.replace(regex, ".");
-
             if(typeof obj[key] == 'object') {
                 myAccumulator.push(nextString);
             }
@@ -47,32 +33,30 @@ async function getUrlObj(region, mobile) {
         return myAccumulator;
     }
 
-    let connectCallback = (x, mongoConnection, currentRegion) => { // separated - grabs urls from mongo
-        let urlsArray = arrayFromUrlObject(x, "", []);
-        mongoConnection.close();
-        console.log('closed the db relationship');
-        // launchPuppeteer(urlsArray, "https://www.buyatoyota.com", "denver", mobile);
-        launchPuppeteer(urlsArray, "https://www.buyatoyota.com", currentRegion, mobile); // will it be able to read mobile?
-    }
+    // let connectCallback = (x, mongoConnection, currentRegion) => { // separated - grabs urls from mongo
+    //     let urlsArray = arrayFromUrlObject(x, "", []);
+    //     mongoConnection.close();
+    //     console.log('closed the db relationship');
+    //     launchPuppeteer(urlsArray, "https://www.buyatoyota.com", currentRegion, mobile); // will it be able to read mobile?
+    // }
 
-    let UrlObj = mongoConnect.retrieveUrlObj(region, connectCallback); // grabs url from Mongo
-    console.log(UrlObj);
+    // let UrlObj = mongoConnect.retrieveUrlObj(region, connectCallback); // grabs url from Mongo
     
     // in the event you wanna re-pull json:
-    // let otherUrlObj = fs.readFile('combined_array.json', 'utf8', function read(err, data) { // used to be generic, but....
-    //      if (err) {
-    //          throw err;
-    //      }
-    //      console.log(data);
-    //      let content = JSON.parse(data);
-    //     //let arrayifiedUrlObj = arrayFromUrlObject(content, "", []);
-    //     // return content;
-    //     // return arrayifiedUrlObj;
-    //     //launchPuppeteer(arrayifiedUrlObj, "https://www.buyatoyota.com/greaterny/");
-    //     //launchPuppeteer(content.urls, "https://www.buyatoyota.com", "denver");
-    //     launchPuppeteer(content.urls, "https://www.buyatoyota.com", region);
-    //                                              // ^^ we can genericize this later ^^
-    // })
+    let otherUrlObj = fs.readFile('combined_array.json', 'utf8', function read(err, data) { // used to be generic, but....
+         if (err) {
+             throw err;
+         }
+         console.log(data);
+         let content = JSON.parse(data);
+        //let arrayifiedUrlObj = arrayFromUrlObject(content, "", []);
+        // return content;
+        // return arrayifiedUrlObj;
+        //launchPuppeteer(arrayifiedUrlObj, "https://www.buyatoyota.com/greaterny/");
+        //launchPuppeteer(content.urls, "https://www.buyatoyota.com", "denver");
+        launchPuppeteer(content.urls, "https://www.buyatoyota.com", region, mobile);
+                                                 // ^^ we can genericize this later ^^
+    })
 }
 
 
@@ -81,8 +65,6 @@ async function launchPuppeteer(urls, mainUrl, region, mobileYes) {
         // {headless: false}  // included in the event we want to watch the action!
         );
     let page = await browser.newPage();
-
-
 // ---------------------------------------------------    mobile    ---------------------------------------------------
 
     if (mobileYes) {
@@ -96,12 +78,9 @@ async function launchPuppeteer(urls, mainUrl, region, mobileYes) {
     }
 
 // ---------------------------------------------------    mobile    ---------------------------------------------------
-
-
     // page.on('dialog', async dialog => { // dunno if we'll ever need this
     //     await dialog.dismiss();
     // })
-
 
     for(let i=0; i < urls.length; i++) {
         try {
@@ -136,9 +115,11 @@ async function launchPuppeteer(urls, mainUrl, region, mobileYes) {
     }
     await browser.close();
 }
+getUrlObj('central', false);
 
-getUrlObj('greaterny', false);
-
+// 'pacificnorthwest', 'central', 'midwest',
+// 'greaterny', 'upstateny', 'connecticut', 
+// 'tristateeast', 'tristate', 'denver'
 
 
 // gets obj from Mongo:
@@ -157,47 +138,6 @@ getUrlObj('greaterny', false);
     // 'tristateeast', 'tristate', 'denver'
 
 
-
-
-
-
-
-
-
-
-// async function URLCapture(urlsArray) {
-//     for(i=0; i < urlsArray.length; i++) {
-//         const page = await browser.newPage();
-//         await page.goto(urlsArray[i]);
-//         await page.screenshot({
-//             path: (urlsArray[i].substring(27, urlsArray[i].length).replace('/', '_')),
-//             // inherently decides adds file
-//             // type: ''
-//         });
-//     }
-// }
-
-// old, working version for one URL: used weird parethases syntax:
-// (async (a) => {
-//     const browser = await puppeteer.launch();
-//     const page = await browser.newPage();
-//     const URLForPicture = 'https://www.buyatoyota.com/greaterny';
-//     await page.goto(URLForPicture);
-//     await page.screenshot({path: URLForPicture.substring(27, URLForPicture.length).replace('/', '_') + '.png'});
-
-//     console.log(a);
-//     // return new Promise((resolve, reject) => {
-//     // })
-//     // .then ({})
-//     // .catch({
-//     //     await browser.close;
-//     // })
-//     // URLCapture(['https://www.buyatoyota.com/greaterny']);
-//     await browser.close();
-//     console.log('it looks like it fired...?');
-// })(); 
-
-// -------------------------------------------------------------------------------------------------------------------------------
 
 
 // puppeteer.launch().then(async browser => {
